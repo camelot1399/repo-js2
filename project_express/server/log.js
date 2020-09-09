3ea71f246404ab9d8a4055f03bd344025fa6b6a8
camelot1399 <camelot1399@mail.ru>
2020-09-09 19:41:25 +0300

    lesson7 fixbug

--- project_express/server/log.js
+++ project_express/server/log.js
@@ -5,13 +5,13 @@ const logger = (action, id, date) => {
 
     const logfile = {action: action, id: id, date: date};
 
-    fs.readFile('logfile.json', 'utf-8', (error, data) => {
+    fs.readFile('stats.json', 'utf-8', (error, data) => {
     if (error) {
         console.log(error);
     } else {
         const log = JSON.parse(data);
         log.push(logfile);
-            fs.writeFile('logfile.json', JSON.stringify(log), (error) => {
+            fs.writeFile('stats.json', JSON.stringify(log), (error) => {
             if (error) {
                 console.log(error);
             }
